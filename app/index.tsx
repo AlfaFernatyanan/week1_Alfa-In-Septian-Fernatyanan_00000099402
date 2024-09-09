b6228a389867bf6bc8ce1f3c2519eebb2dc0ff05
AlfaFernatyanan <alfaifernatyanan@gmail.com>
2024-09-09 20:17:39 +0700

    display name and nim dummy

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,27 @@
-import { Text, View } from "react-native";
+import { Text, View, StyleSheet} from "react-native";
+
+
+const dummyData = {
+    nama : "Alfa In Septian Fernatjanan",
+    nim : "00000099402", 
+    
+}
 
 export default function HomeScreen(){
     return(
-        <View>
-            <Text>Hello World</Text>
+        <View style={styles.container}>
+            <Text>{dummyData.nama}</Text>
+            <Text>{dummyData.nim}</Text>
         </View>
     )
+    
 }
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        padding: 20,
+        margin: 20,
+    }
+})
